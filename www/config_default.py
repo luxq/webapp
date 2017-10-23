4a0d42bbd470d91ed75fa0f9059025ce4fa7e98b
luxq <xueqian1991@icloud.com>
2017-10-23 16:20:04 +0800

    add config.py

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+__author__ = 'xueqian Lu'
+'''
+Default configurations.
+'''
+
+confis = {
+    'db':{
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '%Avl1108',
+        'database': 'test'
+    },
+    'sesstion':{
+        'secret': 'AwEsOmE'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+'''
+Override default configurations.
+'''
+
+__author__ = 'xueqian Lu'
+
+configs = {
+     'db': {
+         'host':'127.0.0.1'
+     }
+}
+
