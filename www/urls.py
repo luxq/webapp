792d51a7c668b88cd1aa99251b61cf535a3b68e0
luxq <xueqian1991@icloud.com>
2017-10-24 11:10:17 +0800

    add apis.

--- www/urls.py
+++ www/urls.py
@@ -7,6 +7,7 @@ import logging
 
 from transwrap.web import get, view
 from models import User, Blog, Comment
+from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
 
 @view('blogs.html')
 @get('/')
@@ -14,3 +15,11 @@ def index():
     blogs = Blog.find_all()
     user = User.find_first('where email=?', 'admin@example.com')
     return dict(blogs=blogs, user=user)
+
+@api
+@get('/api/users')
+def api_get_users():
+    users = User.find_by('order by created_at desc')
+    for u in users:
+        u.password = '******'
+    return dict(users=users)
